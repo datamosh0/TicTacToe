b2f8b3c847fcc03061e3dc19451f1303748c2dc2
datamosh0 <dawsoncontreras@gmail.com>
2022-01-29 15:15:19 -0600

    refactored in react

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import Message from "./Message";
+import Board from "./Board";
+import Restart from "./Restart";
+import "./App.css";
+import { useState } from "react";
+function App() {
+  const [xTurn, setXTurn] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [message, setMessage] = useState("Player X's Turn");
+  const [gameover, setGameover] = useState(false);
+
+  const resetGame = () => {
+    setXTurn(true);
+    setSquares(Array(9).fill(""));
+    setMessage("Player X's turn");
+    setGameover(false);
+  };
+
+  const checkWinner = (board) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let turnHolder = xTurn ? "X" : "O";
+
+    let resultArr = [];
+    winConditions.forEach((combination) => {
+      let row = [
+        board[combination[0]],
+        board[combination[1]],
+        board[combination[2]],
+      ];
+      let results = row.every((currentValue) => currentValue === turnHolder);
+      resultArr.push(results);
+    });
+    if (resultArr.includes(true)) return true;
+  };
+
+  const handleClick = (index) => {
+    let boardTemp = [...squares];
+
+    if (!gameover) {
+      if (boardTemp[index] !== "") return;
+      boardTemp[index] = xTurn ? "X" : "O";
+      setXTurn(!xTurn);
+      setSquares(boardTemp);
+      setMessage(xTurn ? "Player O's turn" : "Player X's turn");
+    }
+
+    if (checkWinner(boardTemp)) {
+      setMessage(xTurn ? "Player X Wins!" : "Player O Wins!");
+      setGameover(true);
+    }
+    if (
+      boardTemp.every((i) => i === "X" || i === "O") &&
+      !checkWinner(boardTemp)
+    ) {
+      setMessage("Tie");
+      setGameover(true);
+    }
+  };
+
+  return (
+    <>
+      <main>
+        <Message msg={message} />
+        <Board className="board" squares={squares} handleClick={handleClick} />
+        <Restart resetGame={resetGame} />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Title from "./Title";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Title />
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
